19a2c7ef7e473e84fe6aa2012b58f6e53af564d3
unknown <joseabilio@gmail.com>
2020-02-27 08:20:08 -0300

    rotina de criar post - separação das hashtags em listas

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -8,7 +8,36 @@ const sharp = require('sharp');
 
 const createPost = (req, res)=>{
     const {author, place, description, hashtags} = req.body;
+    const {filename:image} = req.file;
     
+    await sharp(req.file.path)
+            .resize(500)
+            .jpeg({quality:70})
+            .toFile(
+                path.resolve(req.file.destination, 'resized', image)
+            );
+        fs.unlinkSync(req.file.path);
+    
+    if(hashtags){
+        const itemFind = hashtags.indexOf(' ') !== -1 ? 0 :hashtags.indexOf(',') !== -1 ? 1 : hashtags.indexOf(';') !== -1 ? 2 : 3;
+        switch (itemFind){
+            case 0 : 
+                const listHashTag = hashtags.split(' ');
+                break;
+            case 1 : 
+                const listHashTag = hashtags.split(',');
+                break;
+            case 2 : 
+                const listHashTag = hashtags.split(';');
+                break;
+        }
+     
+    }
+    
+
+
+    
+
     return res.json({ok:true});
 }
 
